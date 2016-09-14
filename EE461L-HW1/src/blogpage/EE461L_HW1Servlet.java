3860f7ccc4a3458365ca96969ad3f331ff85d02d
Yunpeng Zhang <Yunpeng Zhang@Yunpeng>
2016-09-14 12:06:46 -0500

    Initial commit

---
+++ EE461L-HW1/src/blogpage/EE461L_HW1Servlet.java
@@ -0,0 +1,12 @@
+package blogpage;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EE461L_HW1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
