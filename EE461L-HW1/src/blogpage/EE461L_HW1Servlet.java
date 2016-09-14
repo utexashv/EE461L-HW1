ea1d476200e9b32ea4dd913684e4a675e95a4996
Yunpeng Zhang <Yunpeng Zhang@Yunpeng>
2016-09-14 12:32:56 -0500

    initial commit

--- EE461L-HW1/src/blogpage/EE461L_HW1Servlet.java
+++ EE461L-HW1/src/blogpage/EE461L_HW1Servlet.java
@@ -3,10 +3,21 @@ package blogpage;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class EE461L_HW1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        if (user != null) {
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Hello, " + user.getNickname());
+        } else {
+            resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+        }
 	}
 }
---
+++ Guestbook/src/guestbook/OfySignGuestbookServlet.java
@@ -0,0 +1,41 @@
+/**
+ * Peter Yunpeng Zhang
+ * 08/29/2016 EE461L
+ * yz7724
+ * https://peter-ofyguestbook.appspot.com/ofyguestbook.jsp
+ */
+
+package guestbook;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OfySignGuestbookServlet extends HttpServlet {
+	
+	static {
+        ObjectifyService.register(Greeting.class);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+        
+        // objectify
+        String guestbookName = req.getParameter("guestbookName");
+        String content = req.getParameter("content");
+        Greeting gt = new Greeting(user, content);
+        ObjectifyService.ofy().save().entity(gt).now();
+        
+        resp.sendRedirect("/ofyguestbook.jsp?guestbookName=" + guestbookName);
+
+    }
+
+}
